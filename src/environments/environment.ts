3ee53fd6d09f6ef1cb5adf2eb016a1f830698cf0
kanapathiraja <83782249+kanapathiraja@users.noreply.github.com>
2021-06-17 19:56:55 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,12 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   defaultLanguage: 'en',
-  API_URL: 'http://159.69.6.195:3015/api/',
-  STRAPI_URL: 'http://159.69.6.195:3018/',
-  IMG_URL: 'http://159.69.6.195:3018',
-  IMG_URL1: 'http://159.69.6.195:3015'
+  API_URL: 'http://claritaz-poc-alb-987060138.us-west-2.elb.amazonaws.com:3000/api/',
+  STRAPI_URL: 'http://claritaz-poc-alb-987060138.us-west-2.elb.amazonaws.com:1337/',
+  IMG_URL: 'http://claritaz-poc-alb-987060138.us-west-2.elb.amazonaws.com:1337',
+  IMG_URL1: 'http://claritaz-poc-alb-987060138.us-west-2.elb.amazonaws.com:3000'
 };
 
 /*
