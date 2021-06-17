736c8ab38b0de819d86e03d81cbc52c22baf2b35
kanapathiraja <83782249+kanapathiraja@users.noreply.github.com>
2021-06-17 19:31:41 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   defaultLanguage: 'en',
-  API_URL: 'http://18.184.207.46:3001/api/',
-  STRAPI_URL: 'http://18.184.207.46:3018/',
-  IMG_URL: 'http://18.184.207.46:3018',
-  IMG_URL1: 'http://18.184.207.46:3001'
+  API_URL: 'http://claritaz-poc-alb-987060138.us-west-2.elb.amazonaws.com:3000/api/',
+  STRAPI_URL: 'http://claritaz-poc-alb-987060138.us-west-2.elb.amazonaws.com:1337/',
+  IMG_URL: 'http://claritaz-poc-alb-987060138.us-west-2.elb.amazonaws.com:1337',
+  IMG_URL1: 'http://claritaz-poc-alb-987060138.us-west-2.elb.amazonaws.com:3000'
 };
